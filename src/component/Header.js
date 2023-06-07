4767dfba060a1f3c6aa40879f18603ae8da42d11
MuhammadHamzaRiaz <hamza.riaz1431@gmail.com>
2023-06-08 02:04:30 +0500

    link added

--- src/component/Header.js
+++ src/component/Header.js
@@ -5,7 +5,9 @@ function Header() {
   return (
     <div className="header-container">
       <div className="image-container">
+        <a href="https://www.jollofbyjara.com/">
         <img className="header-image" src={logo} alt="logo" />
+        </a>
       </div>
       <div className="menu-container">
         <h1 className="menu">MENU</h1>
