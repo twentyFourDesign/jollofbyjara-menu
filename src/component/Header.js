4a6613cf0e1acdc5312e955243a8df0ba92549ce
24 Design Company <77238502+twentyFourDesign@users.noreply.github.com>
2023-10-01 20:44:50 +0500

    Update Header.js

--- src/component/Header.js
+++ src/component/Header.js
@@ -11,7 +11,7 @@ function Header() {
       </div>
       <div className="menu-container">
         <h1 className="menu">MENU</h1>
-        <p className="menu-description">Click on the menu text for picture and description</p>
+        <p className="menu-description" style={{color:"white"}}>Click on the menu text for picture and description</p>
         <p style={{marginTop:"1rem",color:"white"}}>Dishes are served 'family style' designed for sharing!</p>
         <p style={{marginTop:"10px",color:"white"}}>Prices exclude 5% Consumption Tax (LIRS), 7.5% VAT (FIRS) + 2.5% Service Charge</p>
       </div>
