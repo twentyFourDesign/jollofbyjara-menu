3cb3edb31a3e1b97685cae00e0a3e7f83a732f92
24 Design Company <77238502+twentyFourDesign@users.noreply.github.com>
2024-01-08 15:19:07 +0500

    Update Header.js

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,23 +1,28 @@
 import React from "react";
 import logo from "../image/Jollof by Jara 1.png";
+import HeroImage from "../image/hero.jpg";
 
 function Header() {
   return (
-    <div className="header-container">
-      <div className="image-container">
-        <a href="https://www.jollofbyjara.com/">
-        <img className="header-image" src={logo} alt="logo" />
-        </a>
-      </div>
-      <div className="menu-container">
-        <h1 className="menu">MENU</h1>
-        <p className="menu-description">Click on the menu text for picture and description</p>
-        <p style={{marginTop:"1rem",color:"white"}}>Most dishes are served 'family style' designed for sharing!</p>
-        <p style={{marginTop:"10px",color:"white"}}>Prices exclude 5% Consumption Tax (LIRS), 7.5% VAT (FIRS) + 2.5% Service Charge</p>
-      </div>
+    // <div className="header-container">
+    //   <div className="image-container">
+    //     <a href="https://www.jollofbyjara.com/">
+    //     <img className="header-image" src={logo} alt="logo" />
+    //     </a>
+    //   </div>
+    //   <div className="menu-container">
+    //     <h1 className="menu">MENU</h1>
+    //     <p style={{marginTop:"1rem",color:"white"}}>Jollof By Jara will be reopening for 2024 on Wedenesday 10th January</p>
+    //     <p style={{marginTop:"10px",color:"white"}}>While we apologize for any inconvenience, we have Lots of exciting thing for you</p>
+    //   </div>
+    // </div>
+
+    <div style={{overflow:"hidden"}}>
+      <img src={HeroImage} alt="" style={{width:"100vw",height:"100%"}}/>
     </div>
   );
 }
 
 export default Header;
+
 
