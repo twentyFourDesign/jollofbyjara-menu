2cec0027d8720a5104aaa9b76e759f55e622535e
Kamran <kamranallahyar@outlook.com>
2024-02-15 17:07:00 +0500

    menu api fixes

--- src/component/Menu.js
+++ src/component/Menu.js
@@ -1,69 +1,78 @@
-import React, { useLayoutEffect, useState } from "react";
+import React, {useLayoutEffect, useState} from "react";
 import CardDetails from "./CardDetails";
 import Masonry from "react-smart-masonry";
-const breakpoints = { mobile: 600, tablet: 700, desktop: 1100 };
+
+const breakpoints = {mobile: 600, tablet: 700, desktop: 1100};
 
 export default function Menu() {
-  const [menu, setMenu] = useState([]);
-  const [_, setLoading] = useState(false);
+    const [menu, setMenu] = useState([]);
+    const [_, setLoading] = useState(false);
 
-  const desireOrder = [
-    "Brunch", "Starters / Small Plates", "West African Specials", "Mains",
-    "Sides", "Made for Sharing", "Mya’s Menu (Children's)", "Desserts",
-    "SOFT DRINKS", "MOCKTAILS", "COCKTAILS", "BEER / CIDER / BITTERS",
-    "WINE", "SPIRITS", "PREMIUM COLLECTION", "HOT DRINKS", "OTHER - RESTAURANT",
-    "DELIVERY", "VOUCHERS", "GIFTS", "ART"
-  ];
 
-  useLayoutEffect(() => {
-    setLoading(true);
+    const desireOrder = [
+        "Brunch", "Starters / Small Plates", "West African Specials", "Mains",
+        "Sides", "Made for Sharing", "Mya’s Menu (Children's)", "Desserts",
+        "SOFT DRINKS", "MOCKTAILS", "COCKTAILS", "Cocktails (alc)", "Mocktails (non-alc)", "BEER / CIDER / BITTERS",
+        "WINE", "SPIRITS", "PREMIUM COLLECTION", "HOT DRINKS", "OTHER - RESTAURANT",
+        "DELIVERY", "VOUCHERS", "GIFTS", "ART"
+    ];
+    useLayoutEffect(() => {
+        setLoading(true);
 
-    async function fetchAndSortData() {
-      const apiResponse = await fetch(`https://sabis.jollofbyjara.com/api/`);
-      const apiData = await apiResponse.json();
-      const combinedData = [];
+        async function fetchAndSortData() {
+            const apiResponse = await fetch(`https://sabis.jollofbyjara.com/api/`);
+            const apiData = await apiResponse.json();
+            const combinedData = [];
 
-      for (const sortedItem of desireOrder) {
-        for (const apiItem of apiData.data) {
-          if (sortedItem.toLowerCase() === apiItem.title.toLowerCase()) {
-            combinedData.push({ ...apiItem, category: sortedItem });
-          } else {
-            // Uncomment the following line if you want to log items that don't match any category
-            // console.log(apiItem, 'else');
-          }
+            for (const apiItem of apiData.data) {
+                const index = desireOrder.map(item=> item.toLowerCase()).indexOf(apiItem.title.toLowerCase())
+                if (index > -1) {
+                    combinedData.splice(index, 0, apiItem)
+                } else {
+                    combinedData.push(apiItem)
+                }
+            }
+
+            // for (const sortedItem of desireOrder) {
+            //     for (const apiItem of apiData.data) {
+            //         if (sortedItem.toLowerCase() === apiItem.title.toLowerCase()) {
+            //             combinedData.push({...apiItem, category: sortedItem});
+            //         } else {
+            //             // Uncomment the following line if you want to log items that don't match any category
+            //             // console.log(apiItem, 'else');
+            //         }
+            //     }
+            // }
+
+
+            // combinedData.sort((a, b) => a.title.localeCompare(b.title));
+
+            setMenu(combinedData);
         }
-      }
 
-      
+        fetchAndSortData()
+            .catch(() => setMenu([]))
+            .finally(() => setLoading(false));
+    }, []);
 
-      // combinedData.sort((a, b) => a.title.localeCompare(b.title));
-
-      setMenu(combinedData);
-    }
-
-    fetchAndSortData()
-      .catch(() => setMenu([]))
-      .finally(() => setLoading(false));
-  }, []);
-
-  return (
-    <div className="testing">
-      <Masonry
-        autoArrange={true}
-        breakpoints={breakpoints}
-        columns={{ mobile: 1, tablet: 2, desktop: 3 }}
-        gap={{ mobile: 20, tablet: 30, desktop: 40 }}
-      >
-        {menu?.length ? (
-          menu.map((item, idx) => (
-            <CardDetails title={item.title} items={item.data} key={idx} />
-          ))
-        ) : (
-          <h2 className="data-notfound">Menu Not Found</h2>
-        )}
-      </Masonry>
-    </div>
-  );
+    return (
+        <div className="testing">
+            <Masonry
+                autoArrange={true}
+                breakpoints={breakpoints}
+                columns={{mobile: 1, tablet: 2, desktop: 3}}
+                gap={{mobile: 20, tablet: 30, desktop: 40}}
+            >
+                {menu?.length ? (
+                    menu.map((item, idx) => (
+                        <CardDetails title={item.title} items={item.data} key={idx}/>
+                    ))
+                ) : (
+                    <h2 className="data-notfound">Menu Not Found</h2>
+                )}
+            </Masonry>
+        </div>
+    );
 }
 
 
